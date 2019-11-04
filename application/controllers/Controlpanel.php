9546e604cd60015c5c00f67ad432602e6d3fcdd5
hikmalp <hikmalfachri@github.com>
2019-11-04 19:28:48 +0700

    Update Controlpanel.php

--- application/controllers/Controlpanel.php
+++ application/controllers/Controlpanel.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Controlpanel extends CI_Controller {
+class ControlPanel extends CI_Controller {
 	public function index() {
 		$data = array (
 			"PageContents" => "payrollmanagement/dashboard",
