bbc9bb484c6c387beb78b544bb4f1b33e95ab1a3
hikmalp <hikmalinux@gmail.com>
2019-11-04 13:10:56 +0700

    bbbbbbbb

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'https://ssl123.web.id/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = '';
+$config['index_page'] = 'index.php';
 
 /*
 |--------------------------------------------------------------------------
