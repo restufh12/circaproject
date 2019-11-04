2ffeefd0d46041efd73a5370cf3cc56c98d7af49
hikmalp <hikmalinux@gmail.com>
2019-11-04 15:24:32 +0700

    fix login bug

--- application/controllers/Clogin.php
+++ application/controllers/Clogin.php
@@ -1,7 +1,7 @@
 <?php 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Absenlogin extends CI_Controller {
+class Clogin extends CI_Controller {
 	public function index()
 	{
 		$this->load->view('vlogin');
