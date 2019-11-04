cf32da4a161b54cfca75b317f9ebdb10f8e5bf46
hikmalp <hikmalinux@gmail.com>
2019-11-04 15:28:48 +0700

    fix bug 2.0

--- application/views/vlogin.php
+++ application/views/vlogin.php
@@ -1,12 +1,11 @@
 <!doctype html>
 <html lang="en">
-
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta http-equiv="Content-Language" content="en">
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-  <title><?= $PageTitle ?></title>
+  <title>Login</title>
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no" />
   <meta name="description" content="This is an example dashboard created using build-in elements and components.">
   <meta name="msapplication-tap-highlight" content="no">
@@ -14,35 +13,38 @@
 </head>
 
 <body>
-  <div class="app-container app-theme-white body-tabs-shadow fixed-sidebar fixed-header">
-    <!-- Header -->
-    <?php $this->load->view('templates/header.php') ?>
-
-    <!-- Theme Setting -->
-    <?php $this->load->view('templates/theme_setting.php') ?>
 
 
-    <div class="app-main">
-
-      <!-- Sidebar -->
-      <?php 
-      $dataSidebar['ActiveSidebar'] = $ActiveSidebar;
-
-      ?>
-      <?php $this->load->view('templates/sidebar_'.$ModuleCode.'.php', $dataSidebar) ?>
-
-      <div class="app-main__outer">
-
-        <!-- Div Content -->
-        <?php $this->load->view($PageContents) ?>
-
-        <!-- Div Footer -->
-        <?php $this->load->view('templates/footer.php') ?>
-
+  <div class="container">
+    <div class="card card-login mx-auto text-center bg-dark">
+      <div class="card-header mx-auto bg-dark">
+        <span> <img src="https://amar.vote/assets/img/amarVotebd.png" class="w-75" alt="Logo"> </span><br/>
+        <span class="logo_title mt-5"> Login Dashboard </span>
       </div>
-
+      <div class="card-body">
+        <form action="" method="post">
+          <div class="input-group form-group">
+            <div class="input-group-prepend">
+              <span class="input-group-text"><i class="fas fa-user"></i></span>
+            </div>
+            <input type="text" name="email" class="form-control" placeholder="Username">
+          </div>
+          <div class="input-group form-group">
+            <div class="input-group-prepend">
+              <span class="input-group-text"><i class="fas fa-key"></i></span>
+            </div>
+            <input type="password" name="password" class="form-control" placeholder="Password">
+          </div>
+          <div class="form-group">
+            <input type="submit" name="btn" value="Login" class="btn btn-outline-danger float-right login_btn">
+          </div>
+        </form>
+      </div>
     </div>
   </div>
+
+
+
   <?php $this->load->view('templates/script_src.php') ?>
 </body>
 
