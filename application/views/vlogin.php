07392618bacfc1cdf3ba9910c0cde46b3b8c5d0c
hikmalp <hikmalinux@gmail.com>
2019-11-04 15:33:54 +0700

    sugiono dan cucu

--- application/views/vlogin.php
+++ application/views/vlogin.php
@@ -14,36 +14,39 @@
 
 <body>
 
-
-  <div class="container">
-    <div class="card card-login mx-auto text-center bg-dark">
-      <div class="card-header mx-auto bg-dark">
-        <span> <img src="https://amar.vote/assets/img/amarVotebd.png" class="w-75" alt="Logo"> </span><br/>
-        <span class="logo_title mt-5"> Login Dashboard </span>
+<table style="position: absolute;top: 0px;right: 0px;bottom: 0px;left: 0px;">
+  <tr>
+    <td style="padding: 10% 20% 10%">
+      <div class="container">
+        <div class="card card-login mx-auto text-center bg-dark">
+          <div class="card-header mx-auto bg-dark">
+            <span> <img src="https://amar.vote/assets/img/amarVotebd.png" class="w-75" alt="Logo"> </span><br/>
+            <span class="logo_title mt-5">Login Dashboard</span>
+          </div>
+          <div class="card-body">
+            <form action="" method="post">
+              <div class="input-group form-group">
+                <div class="input-group-prepend">
+                  <span class="input-group-text"><i class="fas fa-user"></i></span>
+                </div>
+                <input type="text" name="email" class="form-control" placeholder="Username">
+              </div>
+              <div class="input-group form-group">
+                <div class="input-group-prepend">
+                  <span class="input-group-text"><i class="fas fa-key"></i></span>
+                </div>
+                <input type="password" name="password" class="form-control" placeholder="Password">
+              </div>
+              <div class="form-group">
+                <input type="submit" name="btn" value="Login" class="btn btn-outline-danger float-right login_btn">
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
-      <div class="card-body">
-        <form action="" method="post">
-          <div class="input-group form-group">
-            <div class="input-group-prepend">
-              <span class="input-group-text"><i class="fas fa-user"></i></span>
-            </div>
-            <input type="text" name="email" class="form-control" placeholder="Username">
-          </div>
-          <div class="input-group form-group">
-            <div class="input-group-prepend">
-              <span class="input-group-text"><i class="fas fa-key"></i></span>
-            </div>
-            <input type="password" name="password" class="form-control" placeholder="Password">
-          </div>
-          <div class="form-group">
-            <input type="submit" name="btn" value="Login" class="btn btn-outline-danger float-right login_btn">
-          </div>
-        </form>
-      </div>
-    </div>
-  </div>
-
-
+    </td>
+  </tr>
+</table>
 
   <?php $this->load->view('templates/script_src.php') ?>
 </body>
