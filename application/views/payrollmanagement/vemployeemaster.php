1db419671e983a3240926d74d94115c766e9d446
hikmalp <hikmalfachri@github.com>
2019-11-05 22:05:02 +0700

    Update vemployeemaster.php

--- application/views/payrollmanagement/vemployeemaster.php
+++ application/views/payrollmanagement/vemployeemaster.php
@@ -1,39 +1,44 @@
 <div class="app-main__inner">
     <?php $this->load->view('templates/breadcrumb.php') ?>
-    <div class="col-sm-12">
-        <div class="main-card mb-3 card">
-            <div class="card-body"><h5 class="card-title">Table with hover</h5>
-                <table class="mb-0 table table-hover table-responsive">
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>First Name</th>
-                            <th>Last Name</th>
-                            <th>Username</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">2</th>
-                            <td>Jacob</td>
-                            <td>Thornton</td>
-                            <td>@fat</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">3</th>
-                            <td>Larry</td>
-                            <td>the Bird</td>
-                            <td>@twitter</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
+    <div class="main-card mb-3 card">
+        <div class="card-body"><h5 class="card-title">Table with hover</h5>
+            <table style="width: 100%" class="mb-0 table table-hover table-responsive">
+                <thead>
+                    <tr>
+                        <th>
+                            <div class="position-relative form-check">
+                                <input name="check" id="exampleCheck" type="checkbox" class="form-check-input">
+                            </div>
+                            <i class="pe-7s-trash"></i> |
+                            <i class="pe-7s-note"></i>
+                        </th>
+                        <th>Employee Code</th>
+                        <th>Employee Name</th>
+                        <th>Last Name</th>
+                        <th>Username</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>Larry</td>
+                        <td>the Bird</td>
+                        <td>@twitter</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </div>
 </div>
